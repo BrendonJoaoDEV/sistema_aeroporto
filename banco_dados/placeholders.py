99b2dc3c3a24a8958d04e2cccb7ee6635b08e85a
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2024-12-06 19:38:04 -0300

    # 005 - Atualização dos placeholders

--- banco_dados/placeholders.py
+++ banco_dados/placeholders.py
@@ -7,30 +7,30 @@
 
 placeholders = {
     'viajante' : '''INSERT INTO viajante 
-                    (id_viajante, nome_viajante, cpf, idade, email)
-                    VALUES (?, ?, ?, ?, ?)''',
+                    (nome_viajante, cpf, idade, email)
+                    VALUES (?, ?, ?, ?)''',
 
     'telefone' : '''INSERT INTO telefone
-                    (id_telefone, id_viajante, telefone)
-                    VALUES (?, ?, ?)''',
-
-    'companhia' : '''INSERT INTO companhia
-                    (id_companhia, nome_companhia)
+                    (id_viajante, telefone)
                     VALUES (?, ?)''',
 
+    'companhia' : '''INSERT INTO companhia
+                    (nome_companhia)
+                    VALUES (?)''',
+
     'aviao' : '''INSERT INTO aviao
-                (id_aviao, id_companhia, nome_aviao, modelo_aviao, 
+                (id_companhia, nome_aviao, modelo_aviao, 
                 ano_fabricacao, numero_voos, numero_assentos)
-                VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                VALUES (?, ?, ?, ?, ?, ?)''',
 
     'rota' : '''INSERT INTO rota
-                (id_rota, cidade_partida, cidade_chegada, duracao)
-                VALUES (?, ?, ?, ?)''',
+                (cidade_partida, cidade_chegada, duracao)
+                VALUES (?, ?, ?)''',
 
     'passagem' : '''INSERT INTO passagem
-                    (id_passagem, id_viajante, id_aviao, id_rota, 
+                    (id_viajante, id_aviao, id_rota, 
                     data_hora_partida, data_hora_chegada, data_hora_volta, 
                     numero_paradas, assentos_disponiveis, preco, 
                     forma_pagamento)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 }

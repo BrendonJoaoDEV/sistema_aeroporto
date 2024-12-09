75d89703a37b0ced6acec327919de66f9180d7c9
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2024-12-09 15:27:22 -0300

    # 013 - Atualização da função de criação

--- crud/criar.py
+++ crud/criar.py
@@ -6,33 +6,39 @@
 
 # Definição da função:
 def criar_registro(tabela, dados):
-    # Importção da biblioteca sqlite e os placeholders:
+    # Importação da biblioteca sqlite e os placeholders:
     import sqlite3
-    from banco_dados.placeholders import placeholders
+    from banco_dados.placeholders import inserts
 
     # Criação de uma conexão com o banco e um cursor para manipulação:
-    conexao = sqlite3.connect("banco_dados/bancos_dados.db")
+    conexao = sqlite3.connect('banco_dados/bancos_dados.db')
     cursor = conexao.cursor()
 
     # Verificão de qual tabela será usada para agir de acordo:
     if tabela == 'viajante':
-        cursor.executemany(placeholders['viajante'], dados)
+        cursor.executemany(inserts['viajante'], dados)
         resposta = 'Inserção concluída'
+
     elif tabela == 'telefone':
-        cursor.executemany(placeholders['telefone'], dados)
+        cursor.executemany(inserts['telefone'], dados)
         resposta = 'Inserção concluída'
+
     elif tabela == 'companhia':
-        cursor.executemany(placeholders['companhia'], dados)
+        cursor.executemany(inserts['companhia'], dados)
         resposta = 'Inserção concluída'
+
     elif tabela == 'aviao':
-        cursor.executemany(placeholders['aviao'], dados)
+        cursor.executemany(inserts['aviao'], dados)
         resposta = 'Inserção concluída'
+
     elif tabela == 'rota':
-        cursor.executemany(placeholders['rota'], dados)
+        cursor.executemany(inserts['rota'], dados)
         resposta = 'Inserção concluída'
+
     elif tabela == 'passagem':
-        cursor.executemany(placeholders['passagem'], dados)
+        cursor.executemany(inserts['passagem'], dados)
         resposta = 'Inserção concluída'
+
     else:
         resposta = 'Falha na inserção: tabela não encontrada'
 

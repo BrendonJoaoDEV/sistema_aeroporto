fbb714ed39efe510abd0a62fce33a71be4b2f8d7
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2024-12-10 21:14:21 -0300

    # 025 - Placeholders para selects padrão

--- banco_dados/placeholders.py
+++ banco_dados/placeholders.py
@@ -74,6 +74,54 @@ updates = {
                     WHERE id_passagem = ?'''
 }
 
-# Não existem placeholders para os selects pois
-# Eles poderam ser construídos de diversas formas
-# No módulo visualizar.
+selects = {
+    'viajante': '''SELECT 
+                    id_viajante AS "id",
+                    nome_viajante AS "nome",
+                    cpf AS "cpf",
+                    idade,
+                    email
+                    FROM viajante''',
+
+    'telefone': '''SELECT 
+                    id_telefone AS "id",
+                    id_viajante AS "id viajante",
+                    telefone
+                    FROM telefone''',
+
+    'companhia': '''SELECT
+                    id_companhia AS "id",
+                    nome_companhia AS "nome"
+                    FROM companhia''',
+
+    'aviao': '''SELECT
+                id_aviao AS "id",
+                id_companhia AS "id companhia",
+                nome_aviao AS "nome",
+                modelo_aviao AS "modelo",
+                ano_fabricacao AS "ano de fabricação",
+                numero_voos AS "número de voos",
+                numero assentos AS "número de assentos"
+                FROM aviao''',
+
+    'rota': '''SELECT
+                id_rota AS "id",
+                cidade_partida AS "cidade de partida",
+                cidade_chegada AS "cidade de chegada",
+                duracao AS "duração"
+                FROM rota''',
+
+    'passagem': '''SELECT
+                    id_passagem AS "id",
+                    id_viajante AS "id do viajante",
+                    id_aviao AS "id do avião",
+                    id_rota AS "id da rota",
+                    data_hora_partida AS "data e hora de partida",
+                    data_hora_chegada AS "data e hora de chegada",
+                    data_hora_volta AS "dara e hora de volta",
+                    numero_paradas AS "número de paradas",
+                    assentos_disponiveis AS "assentos disponíveis"
+                    preco AS "preço"
+                    forma_pagamento AS "forma de pagamento"
+                    FROM passagem'''
+}

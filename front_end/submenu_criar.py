805ab6791c591f21cfc16da91ef283fa17162d92
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2024-12-11 21:18:28 -0300

    # 031 - Continuação do submen u_criar

--- front_end/submenu_criar.py
+++ front_end/submenu_criar.py
@@ -1,16 +1,14 @@
 # Curso de Desenvolvimento de Sistemas.
 # Turma 0152.
 # Professor: Brendon João Campos Neves.
-# Data: 010/12/2024.
-# Módulo onde será definida a função com código referente a entrada de dados
-# para a opção número 1 do menu principal.
+# Data: 11/12/2024.
+# Módulo onde será definida a função que recebe e trata os dados da opção criar do menu principal.
 
 # Definição da função:
 def opcao_1():
-    # Importação do PrettyTable e do sqlite:
-    import sqlite3
-    from prettytable import PrettyTable
-    
+    # Importação da função exibir_tabela:
+    from front_end.exibir_tabela import exibir_tabela
+
     # Declaração de variáveis auxiliares e de retorno:
     tabela = ''
     dados = []
@@ -29,7 +27,7 @@ def opcao_1():
         print('6 - Rota')
 
         # Escolhe da tabela
-        opcao = input('Digite o númeroda opção que deseja: ').strip()
+        opcao = input('Digite o número da opção que deseja: ').strip()
 
         # Opção que volta para o menu principal:
         if opcao == '0':
@@ -44,21 +42,8 @@ def opcao_1():
                 nome = input('Digite o nome do viajante: ').strip().lower()
 
                 cpf = input('Digite o cpf do viajante: ').strip().lower()
-                # Validando e removendo . e - do cpf:
-                if '.' in cpf or '-' in cpf:
-                    cpf = cpf.replace('.', '') and cpf.replace('-', '')
-                    if cpf.isdigit():
-                        cpf = int(cpf)
-                    else:
-                        print('cpf inválido, digite apenas números')
-                        continue
 
                 idade = input('Digite a idade do viajante: ').strip().lower()
-                if idade.isdigit():
-                    idade = int(idade)
-                else:
-                    print('Idade inválida, digite apenas números')
-                    continue
 
                 email = input('Digite o email do viajante: ').strip().lower()
 
@@ -78,7 +63,33 @@ def opcao_1():
 
         # Inserção na tabela telefone:
         elif opcao == '2':
-            pass
+            tabela = 'telefone'
+
+            # Looping de inserção de dados:
+            while True:
+                # Exibe a tabela viajante para permitir a correta relação entre viajante e telefone:
+                exibir_tabela('viajante')
+
+                id_viajante = input(
+                    'Digite o id do viajante dono do telefone: '
+                ).strip().lower()
+
+                telefone = input(
+                    'Digite o número de telefone: ').strip().lower()
+
+                # Salvando uma copia das informações em dados e limpando informacao:
+                informacao = (id_viajante, telefone)
+                dados.append(informacao)
+                informacao = ()
+
+                # Dando ao usuário a opção de adicionar mais dados:
+                opcao = input(
+                    'Deseja inserir mais dados na tabela viajante (s/n): '
+                ).strip().lower()
+                if opcao == 's':
+                    continue
+                else:
+                    break
 
         # Inserção na tabela companhia:
         elif opcao == '3':
@@ -97,7 +108,7 @@ def opcao_1():
                 # Dando ao usuário a opção de adicionar mais dados:
                 opcao = input(
                     'Deseja adicionar mais dados na tabela companhia (s/n): '
-                    ).strip().lower()
+                ).strip().lower()
                 if opcao == 's':
                     continue
                 else:
@@ -105,12 +116,49 @@ def opcao_1():
 
         # Inserção na tabela aviao:
         elif opcao == '4':
-            pass
+            tabela = 'aviao'
+
+            # Looping de inserção de dados:
+            while True:
+                # Exibe a tabela companhia para permitir a correta relação entre companhia e aviao:
+                exibir_tabela('companhia')
+
+                id_companhia = input(
+                    'Digite o id da companhia dona do avião: '
+                    ).strip().lower()
+
+                nome = input('Digite o nome do avião: ').strip().lower()
+
+                modelo = input('Digite o modelo do avião: ').strip().lower()
+
+                ano_fabricacao = input(
+                    'Digite o ano de fabricação do avião: ').strip().lower()
+
+                numero_voos = input(
+                    'Digite o número de voos do avião: ').strip().lower()
+
+                numero_assentos = input(
+                    'Digite o número de assentos: ').strip().lower()
+
+                # Salvando uma copia das informações em dados e limpando informacao:
+                informacao = (id_companhia, nome, modelo,
+                              ano_fabricacao, numero_voos, numero_assentos)
+                dados.append(informacao)
+                informacao = ()
+
+                # Dando ao usuário a opção de adicionar mais dados:
+                opcao = input(
+                    'Deseja adicionar mais dados na tabela avião (s/n): '
+                    ).strip().lower()
+                if opcao == 's':
+                    continue
+                else:
+                    break
 
         # Inserção na tabela passagem:
         elif opcao == '5':
             pass
-        
+
         # Inserção na tabela rota
         elif opcao == '6':
             pass
